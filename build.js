24f937b3393b89eb9921991a79ca7747017ffcbe
Anton Korzunov <thekashey@gmail.com>
2018-11-21 21:45:50 +1100

    initial build

---
+++ build.js
@@ -0,0 +1,16 @@
+const {writeFileSync, readFileSync} = require('fs');
+const pkg = require('react-dom/package');
+const self = require('./package');
+const patch = require('./patch');
+
+self.version = pkg.version;
+
+writeFileSync('package.json', JSON.stringify(self, null, 2));
+
+const patched = patch(readFileSync('./node_modules/react-dom/cjs/react-dom.development.js').toString())
+if (!patched) {
+  throw new Error('could not patch react-dom')
+}
+
+writeFileSync('index.js', patched);
+
